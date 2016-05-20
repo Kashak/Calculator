12d798c7f357dee538d131631c9fa08bfc5dc4e1
Sam Fisher <samfisher@Sams-MacBook-Pro.local>
2016-05-20 06:41:16 -0400

    First raw version

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,108 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Sam Fisher on 5/16/16.
+//  Copyright © 2016 Sam Fisher. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain {
+    
+    private enum Op: CustomStringConvertible {
+        case Operand(Double)
+        case UnaryOperation(String, (Double) -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        
+        var description: String {
+            get {
+                switch self {
+                case .Operand(let val): return "\(val)"
+                case .UnaryOperation(let operation, _): return operation
+                case .BinaryOperation(let operation, _): return operation
+                }
+            }
+        }
+    }
+    
+    private var opStack = [Op]()
+    
+    private var knownOps = [String: Op]()
+    
+    typealias PropertyList = AnyObject
+    
+    var program: PropertyList { // guaranteed to be a PropertyList
+        get {
+            return opStack.map { $0.description }
+        }
+        set {
+            if let opSymbols = newValue as? Array<String> {
+                var newOpStack = [Op]()
+                for opSymbol in opSymbols {
+                    if let op = knownOps[opSymbol] {
+                        newOpStack.append(op)
+                    } else if let operand = NSNumberFormatter().numberFromString(opSymbol)?.doubleValue {
+                        newOpStack.append(.Operand(operand))
+                    }
+                }
+                opStack = newOpStack
+            }
+        }
+    }
+    
+    init() {
+        func learnOperation(op: Op) {
+            knownOps[op.description] = op
+        }
+        learnOperation( Op.BinaryOperation("✖️", *) )
+        learnOperation( Op.BinaryOperation("➗", { $1 / $0 }) )
+        learnOperation( Op.BinaryOperation("➕", +) )
+        learnOperation( Op.BinaryOperation("➖", { $1 - $0 }) )
+        learnOperation( Op.UnaryOperation("sqrt", sqrt) )
+    }
+
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            
+            switch op {
+            case .Operand(let operand):
+                return (operand, remainingOps)
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand =  operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+        return (nil, ops)
+    }
+
+    func evaluate() -> Double? {
+        let (result, remainder) = evaluate(opStack)
+        print("\(opStack) = \(result) with \(remainder) left over")
+        return result
+    }
+    
+    func pushOperand(operand: Double) -> Double? {
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    func performOperation(symbol: String) -> Double? {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+        return evaluate()
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    var userIsInTheMiddleOfTyping = false
+    var brain = CalculatorBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
 
+    @IBAction func enter() {
+        userIsInTheMiddleOfTyping = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTyping = false
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            enter()
+        }
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+        
+    }
 }
 
